04df509848e38d34619bdd8fff9ac37eef1d57bf
Dieu-Linh12M <ng.tr.dieu.linh1502@gmail.com>
2025-04-03 21:53:28 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pass.value
+  if (u=="diulin" && p=="1502")
+  {
+    window.open("contents/home.html")
+    documet.getElementById(compliler).style.display="block"
+  }
+}  
